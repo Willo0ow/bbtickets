8ea9d1e4e332ca5b0c06fa787e137bc0953129e9
Willo0ow <dhinc39@gmail.com>
2021-03-03 01:12:27 +0100

    dragged task - update status in database

--- resources/js/mixins/dragNdrop.js
+++ resources/js/mixins/dragNdrop.js
@@ -1,4 +1,4 @@
-
+import axios from '../axios'
 export default {
     methods:{
         allowDrop(event) {
@@ -10,7 +10,12 @@ export default {
             this.draggedTaskIndex = index
             this.draggedTaskCurrentStatus = currentStatus
         },
-        drop(event) {
+        async assignNewStatus({status, ticketId}){
+            const form = {status}
+            const tlog = {ticket: ticketId, variable: 'status', value: status, correction: true}
+            await axios.patch(`/ticket/${ticketId}`, {ticket: form, tlog:tlog})
+        },
+        async drop(event) {
             event.preventDefault();
             const data = event.dataTransfer.getData("text");
             const closestStatus = event.target.closest('.status-body')
@@ -18,6 +23,8 @@ export default {
             this.draggedTask.status = status
             this.tasksByStatus[this.draggedTaskCurrentStatus].splice(this.draggedTaskIndex, 1)
             this.tasksByStatus[status].push(this.draggedTask)
+            await this.assignNewStatus({status, ticketId: this.draggedTask.id})
+
             this.draggedTask = null
             this.draggedTaskIndex = null
             this.draggedTaskCurrentStatus = null
